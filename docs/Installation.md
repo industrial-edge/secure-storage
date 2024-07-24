409d4ec843ef41a0b89751df52eb7297ac450e5f
Milan Hurban <milan.hurban@siemens.com>
2024-07-24 12:14:36 +0000

    update to API v2.0.0

--- docs/Installation.md
+++ docs/Installation.md
@@ -1,50 +1,48 @@
 # Installation
 
 - [Installation](#installation)
-  - [Build application](#build-application)
-    - [Cloning image](#cloning-image)
-    - [Build docker image](#build-docker-image)
-  - [Upload Secure Storage App to the Industrial Edge Managment](#upload-secure-storage-app-to-the-industrial-edge-managment)
+  - [Build the application](#build-the-application)
+    - [Cloning the repository](#cloning-the-repository)
+    - [Building the docker image](#building-the-docker-image)
+  - [Upload the Secure Storage App to the Industrial Edge Management](#upload-the-secure-storage-app-to-the-industrial-edge-management)
     - [Connect your Industrial Edge App Publisher](#connect-your-industrial-edge-app-publisher)
     - [Upload Secure Storage App using the Industrial Edge App Publisher](#upload-secure-storage-app-using-the-industrial-edge-app-publisher)
-  - [Deploying of App](#deploying-of-app)
+  - [Deploying the App](#deploying-the-app)
 
-## Build application
+## Build the application
 
-### Cloning image
+### Cloning the repository
 
 - Clone or Download the source code to your engineering VM
 
-### Build docker image
+### Building the docker image
 
-- Open console in the source code folder
-- Use command `docker compose build` to create the docker image.
+- Open the console in the source code folder
+- Use the command `docker compose build` to create the docker image
 - This docker image can now be used to build you app with the Industrial Edge App Publisher
 - `docker images | grep secure-storage-app` can be used to check for the images
-- You should get a result similiar to this:
+- You should get a result similar to this:
 
   ![deploy VFC](./graphics/docker_images_securestorageapp.png)
 
-## Upload Secure Storage App to the Industrial Edge Managment
-
-Please find below a short description how to publish your application in your IEM.
+## Upload the Secure Storage App to the Industrial Edge Management
 
 For more detailed information please see the section for [uploading apps to the IEM](https://github.com/industrial-edge/upload-app-to-iem).
 
 ### Connect your Industrial Edge App Publisher
 
 - Connect your Industrial Edge App Publisher to your docker engine
-- Connect your Industrial Edge App Publisher to your Industrial Edge Managment System
+- Connect your Industrial Edge App Publisher to your Industrial Edge Management System
 
 ### Upload Secure Storage App using the Industrial Edge App Publisher
 
-- Create a new application using the Industrial Publisher
+- Create a new application using the Industrial Edge App Publisher
 - Add a new app version
 - Import the [docker-compose](../docker-compose.yml) file using the **Import YAML** button
-- The warning `Build (services >> scanner-service) is not supported` can be ignored
-- **Start Upload** to transfer the app to Industrial Edge Managment
+- The warning `Build (services >> secure-storage-service) is not supported` can be ignored
+- **Start Upload** to transfer the app to Industrial Edge Management
 - Further information about using the Industrial Edge App Publisher can be found in the [IE Hub](https://iehub.eu1.edge.siemens.cloud/documents/appPublisher/en/start.html)
 
-## Deploying of App
+## Deploying the App
 
-Select the Uploaded version of the Application and Install it to the desired Industrial Edge Device.
+Select the uploaded version of the application and install it to the desired Industrial Edge Device.
--- src/main.py
+++ src/main.py
@@ -2,62 +2,111 @@ from flask import Flask, render_template, request, Response
 import json
 import os
 import requests
-from werkzeug.middleware.proxy_fix import ProxyFix
+import spiffe
+import cryptography
+
+requests.packages.urllib3.disable_warnings()  # ignore insecure connection warnings
+
+BASE_URL = "https://edge-iot-core.proxy-redirect:8443/b.service/api/v2/secure-storage"
+SOCKET_PATH = "unix:/var/run/edgedevice/edgeapiagent.sock"
+SPIFFE_ID = os.environ.get("EDGE_SPIFFE_ID", "spiffe://iert.siemens.com/edge-service")
+
+x509_source = spiffe.X509Source(socket_path=SOCKET_PATH)
+
+# get x509 certificate and JSON Web Token for Secure Storage API
+with spiffe.WorkloadApiClient(socket_path=SOCKET_PATH) as client:
+    try:
+        x509_svid = client.fetch_x509_svid()
+        jwt_svid = client.fetch_jwt_svid(audience=[SPIFFE_ID])
+    except spiffe.exceptions.WorkloadApiError as e:
+        print(f"SPIFFE Workload API Error: {e}")
+
+# certificate and key have to be saved to file as Python requests module doesn't accept raw input
+with open("tmp/cert.pem", "wb") as f:
+    f.write(
+        x509_svid.cert_chain[0].public_bytes(
+            encoding=cryptography.hazmat.primitives.serialization.Encoding.PEM
+        )
+    )
+
+with open("tmp/key.key", "wb") as f:
+    f.write(
+        x509_svid.private_key.private_bytes(
+            encoding=cryptography.hazmat.primitives.serialization.Encoding.PEM,
+            format=cryptography.hazmat.primitives.serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=cryptography.hazmat.primitives.serialization.NoEncryption(),
+        )
+    )
 
 app = Flask(__name__)
 app._static_folder = os.path.abspath("templates/static/")
 
-with open("/var/run/edgedevice/certsips.json", 'r') as certsip:
-    data = json.load(certsip)
-    DEVICE_IP = data["edge-ips"]
-    API_PATH = data["secure-storage-api-path"]
-
-
-BASE_URL = "https://" + DEVICE_IP + API_PATH
 HEADERS = {
-    'accept': '*/*',
-    'Content-Type': 'application/json'
+    "accept": "*/*",
+    "Content-Type": "application/json",
+    "Authorization": f"JWT {jwt_svid.token}",
 }
 
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route("/members", methods=['GET', 'POST'])
+@app.route("/members", methods=["GET", "POST"])
 def members():
     key_value_pairs = []
-    if request.method == 'GET':
+    if request.method == "GET":
         response = requests.request(
-            "GET", (BASE_URL + "/keys"), headers=HEADERS, verify=False).json()
+            "GET",
+            (BASE_URL + "/keys"),
+            headers=HEADERS,
+            cert=("tmp/cert.pem", "tmp/key.key"),
+            verify=False,
+        ).json()
+
         if not response["securestoragekeys"]:
             return Response({})
         for element in response["securestoragekeys"]:
             response_call = requests.request(
-                "GET", (BASE_URL + "/keys/" + element), headers=HEADERS, verify=False).json()
+                "GET",
+                (BASE_URL + "/keys/" + element),
+                headers=HEADERS,
+                cert=("tmp/cert.pem", "tmp/key.key"),
+                verify=False,
+            ).json()
             key_value_pairs.append(response_call)
         return Response(json.dumps(key_value_pairs))
 
-    if request.method == 'POST':
+    if request.method == "POST":
         data = json.loads(request.data)
         body = json.dumps(data)
-        if not 'key' in data or not 'value' in data or data['key'] == '' or data['value'] == '':
-            return Response(json.dumps({
-                "errors": [
-                    {
-                        "message": "You must specify key and value"
-                    }
-                ]
-            }), status=400)
+        if (
+            not "key" in data
+            or not "value" in data
+            or data["key"] == ""
+            or data["value"] == ""
+        ):
+            return Response(
+                json.dumps({"errors": [{"message": "You must specify key and value"}]}),
+                status=400,
+            )
 
         response_post = requests.request(
-            "POST", (BASE_URL + "/keys"), data=body, headers=HEADERS, verify=False)
+            "POST",
+            (BASE_URL + "/keys"),
+            data=body,
+            headers=HEADERS,
+            cert=("tmp/cert.pem", "tmp/key.key"),
+            verify=False,
+        )
         if response_post.status_code == 201:
-            return Response('Successfully added a member', status=201)
+            return Response("Successfully added a member", status=201)
         else:
-            return Response(json.dumps(response_post.json()), status=response_post.status_code)
+            return Response(
+                json.dumps(response_post.json()), status=response_post.status_code
+            )
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=False)
